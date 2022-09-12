1d6a6ce28e42b0878af4515b30a5b7c026dd7cea
SEUNGJOOMOON <nick901106@gmail.com>
2022-09-12 14:48:26 +0900

    타임리프 추가

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+
+		System.out.println(hello.getData());
+
 		SpringApplication.run(JpashopApplication.class, args);
+
+
 	}
 
 }
