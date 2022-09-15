b50caacffe93c6a7fc1628126ee845cd41eb0874
SEUNGJOOMOON <nick901106@gmail.com>
2022-09-15 22:04:49 +0900

    h2 디비 추가, 테스트케이스 추가, 멤버 repository 추가

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository //컴포넌트 스캔 대상으로 자동으로 bean에 등록됨.
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // jpa 설정으로 자동으로 등록됨.
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
