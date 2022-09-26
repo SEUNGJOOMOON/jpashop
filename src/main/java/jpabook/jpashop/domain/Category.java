5cbb391053479d4a7a112ef55d990695ea7bf2a3
SEUNGJOOMOON <nick901106@gmail.com>
2022-09-26 21:30:35 +0900

    카테고리 domain 추가

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    ) // 중간 테이블 mapping(실전에서는 거의 안쓴다.)
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent") // 셀프 양방향 관계
+    private List<Category> child = new ArrayList<>();
+}
