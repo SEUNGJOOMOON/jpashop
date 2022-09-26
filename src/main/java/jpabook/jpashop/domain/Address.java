d3dec227c923b282de502779a1afe3dc256d8360
SEUNGJOOMOON <nick901106@gmail.com>
2022-09-26 21:29:55 +0900

    카테고리 domain 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -4,15 +4,23 @@ import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
-@Embeddable //JPA 내장객체
+@Embeddable //JPA 내장객체(셋터는 제거하고 생성자를 통해 데이터를 넣게 해야한다.)
+// public 또는 protected만 사용해야 함(jpa 스펙)
 @Getter
 public class Address {
 
     private String city;
-
     private String street;
-
     private String zipcode;
 
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
 }
